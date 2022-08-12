73c7101bd38e958714a58eb48f61b6240490d77f
ZuhraMuk <zmu3842@gmail.com>
2022-08-12 23:17:10 +0300

    first commit

---
+++ вакансии.js
@@ -0,0 +1,108 @@
+
+function playdo(){
+    var до=new Audio;
+    до.src="zvuk/do.mp3";
+    до.play();
+}
+$(function(){ $('#do').mouseenter(function(){
+    playdo();
+});});
+
+function playla(){
+    var ля=new Audio;
+    ля.src="zvuk/la.mp3";
+    ля.play();
+}
+$(function(){ $('#la').mouseenter(function(){
+    playla();
+});});
+
+function playmi(){
+    var ми=new Audio;
+    ми.src="zvuk/mi.mp3";
+    ми.play();
+}
+$(function(){ $('#mi').mouseenter(function(){
+    playmi();
+});});
+
+function playre(){
+    var ре=new Audio;
+    ре.src="zvuk/re.mp3";
+    ре.play();
+}
+$(function(){ $('#re').mouseenter(function(){
+    playre();
+});});
+
+function playsol(){
+    var соль=new Audio;
+    соль.src="zvuk/sol.mp3";
+    соль.play();
+}
+$(function(){ $('#sol').mouseenter(function(){
+    playsol();
+});});
+
+function playb_si(){
+    var бси=new Audio;
+    бси.src="zvuk/b_si.mp3";
+    бси.play();
+}
+$(function(){ $('#b_si').mouseenter(function(){
+    playb_si();
+});});
+
+function playb_la(){
+    var бла=new Audio;
+    бла.src="zvuk/b_la.mp3";
+    бла.play();
+}
+$(function(){ $('#b_la').mouseenter(function(){
+    playb_la();
+});});
+
+function playb_mi(){
+    var бми=new Audio;
+    бми.src="zvuk/b_mi.mp3";
+    бми.play();
+}
+$(function(){ $('#b_mi').mouseenter(function(){
+    playb_mi();
+});});
+
+function playb_re(){
+    var бре=new Audio;
+    бре.src="zvuk/b_re.mp3";
+    бре.play();
+}
+$(function(){ $('#b_re').mouseenter(function(){
+    playb_re();
+});});
+
+function playb_do(){
+    var бдо=new Audio;
+    бдо.src="zvuk/b_do.mp3";
+    бдо.play();
+}
+$(function(){ $('#b_do').mouseenter(function(){
+    playb_do();
+});});
+
+function playb_sol(){
+    var бсоль=new Audio;
+    бсоль.src="zvuk/b_sol.mp3";
+    бсоль.play();
+}
+$(function(){ $('#b_sol').mouseenter(function(){
+    playb_sol();
+});});
+
+function playb_fa(){
+    var бфа=new Audio;
+    бфа.src="zvuk/b_fa.mp3";
+    бфа.play();
+}
+$(function(){ $('#b_fa').mouseenter(function(){
+    playb_fa();
+});});
